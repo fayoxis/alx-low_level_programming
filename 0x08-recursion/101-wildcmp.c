bfd71b4f433bed4a47b274b7881cb61c19567f9b
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-10-25 10:23:53 +0100

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,10 +1,14 @@
 #include "main.h"
 
 /**
- * wildcmp - Compare strings with wildcards
- * @str1: Pointer to the first string
- * @str2: Pointer to the second string
- * Return: 1 if the strings match, 0 otherwise
+ * wildcmp - Compare strings with wildcards.
+ * @str1: Pointer to the first string.
+ * @str2: Pointer to the second string.
+ * This function compares two strings with the support of wildcards.
+ * It recursively checks if the strings match,
+ * taking into account the wildcard character '*'.
+ * If the strings match, it returns 1. Otherwise, it returns 0.
+ * Return: 1 if the strings match, 0 otherwise.
  */
 int wildcmp(char *str1, char *str2)
 {
