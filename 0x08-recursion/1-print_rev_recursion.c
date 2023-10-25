8cefd70378bdd0998da43bc21ad099c1a955e273
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-10-25 10:26:37 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,13 +4,11 @@
  * _print_rev_recursion - Recursively prints a string
  * in reverse order.
  * @str: The string to be reversed.
- *
  * This function takes a string and recursively
  * prints it in reverse order.It moves to the next
  * character and calls itself recursively.
  * After the recursive call, it moves back to the
  * previous character and prints it.
- * 
  * Return: None.
  */
 void _print_rev_recursion(char *str)
