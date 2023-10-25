c1807e0c150116852a5d89ba8012e72f0a9b388c
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-10-25 10:07:17 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,10 +1,14 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - Calculate the length of a string recursively
- * @str: The string to count
- *
- * Return: The length of the string
+ * _strlen_recursion - Recursively calculates
+ * the length of a string.
+ * @str: The string to be counted.
+ * This function recursively counts the length of a given string.
+ * It moves to the next character and calls itself recursively.
+ * The base case is when the string reaches the null terminator,
+ * indicating the end of the string.
+ * Return: The length of the string.
  */
 int _strlen_recursion(char *str)
 {
