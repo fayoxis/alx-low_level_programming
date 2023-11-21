1eb1599f50f7e321b98a40c7a3c9c52b84c517ca
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-11-21 13:19:29 +0100

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,8 +19,8 @@ struct listint_s *next;
 }
 listint_t;
 
-size_t print_listint(const listint_t *w);
-size_t listint_len(const listint_t *w);
+size_t print_listint(const listint_t *head);
+size_t listint_len(const listint_t *head);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
