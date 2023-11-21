7cc965279876423fa9507fb6d278c47be580d173
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-11-21 13:25:29 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,9 +7,11 @@
 * @head: The head of the linked list in your code
 * Return: The number of nodes of the linked list in your code
 **/
-size_t print_listint(const listint_t *head) {
+size_t print_listint(const listint_t *head)
+{
 size_t count = 0;
-while (head != NULL) {
+while (head != NULL)
+{
 printf("%d\n", head->n);
 head = head->next;
 count++;
