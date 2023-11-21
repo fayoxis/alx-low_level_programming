f44885eea352a205bf4e031c8ef3065eb40c7234
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-11-21 23:58:29 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,14 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
+
 /**
-* insert_nodeint_at_index - inserts a node at  given position
-* @head: double pointer to the head of node
-* @idx: index of list where the node should be adde
-* @n: ...data to enter
-*
-* Return: address of the node, NULL if failed
-*/
+ * insert_nodeint_at_index - inserts a node at a given position
+ * @head: double pointer to the head of the node
+ * @idx: index of the list where the node should be added
+ * @n: data to be entered
+ *
+ * Return: address of the node, NULL if failed
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 listint_t *new_node, *act_node;
@@ -21,27 +22,25 @@ return (NULL);
 new_node->n = n;
 if (idx == 0)
 {
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
-return (NULL);
-
-new_node->n = n;
 new_node->next = *head;
 *head = new_node;
 return (new_node);
 }
+
 act_node = *head;
-for (j = 0; j < idx - 1 && act_node != NULL; j++)
+j = 0;
+while (j < idx - 1 && act_node != NULL)
+{
 act_node = act_node->next;
+j++;
+}
 if (act_node == NULL)
 return (NULL);
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
-return (NULL);
-new_node->n = n;
 new_node->next = act_node->next;
 act_node->next = new_node;
+
 return (new_node);
 }
+
 return (NULL);
 }
