6284de5827dfe60f48da1f8116acbff9bd59f566
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-11-21 13:18:28 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,22 +3,17 @@
 
 
 /**
-* print_list_integers - Prints all the elements of a linked list mark it corect
+* print_listint - Prints all the elements of a linked list mark it corect
 * @head: The head of the linked list in your code
 *
 * Return: The number of nodes of the linked list in your code
 */
-size_t print_list_integers(const listint_t *head)
-{
-    size_t num_elements = 0;
-
-    while (head != NULL)
-    {
-        printf("%d\n", head->n);
-        head = head->next;
-        num_elements++;
-    }
-
-    return num_elements;
+size_t print_listint(const listint_t *head) {
+size_t count = 0;
+while (head != NULL) {
+printf("%d\n", head->n);
+head = head->next;
+count++;
 }
-
+return (count);
+}
