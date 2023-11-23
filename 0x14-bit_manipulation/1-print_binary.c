cc2b41588922151aeb075b52d55905fcdfb75928
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-11-23 11:35:51 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,32 +4,27 @@
 #include "main.h"
 
 /**
- * print_binary - Prints an unsigned long interger in binary format.
+ * print_binary - Prints the binary representation of an unsigned long integer.
  *
- * @num:  n to be printed in binary.
+ * @num: Number to be printed in binary.
  *
- * Return: return void
+ * Return: void
  */
 void print_binary(unsigned long int num)
 {
 unsigned long int printbit = 1ul << 63;
-char w = '0';
-
-
-/* Find the  non-zero bit */
-while (!(printbit & num) && printbit != 0)
+/* Find the non-zero bit */
+while (printbit > 0 && !(printbit & num))
 printbit = printbit >> 1;
 if (printbit == 0)
-putchar(w);
+putchar('0');
+
 /* Print binary representation  */
-while (printbit)
+for (; printbit > 0; printbit = printbit >> 1)
 {
 if (printbit & num)
-w = '1';
+putchar('1');
 else
-w = '0';
-write(1, &w, 1);
-printbit = printbit >> 1;
+putchar('0');
 }
-
 }
