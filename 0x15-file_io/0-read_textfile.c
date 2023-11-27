929a0daffabc80546d8126d827aedc8280e6feb4
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-11-27 14:16:08 +0100

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,35 +12,29 @@ size_t read_textfile(const char *f_name, size_t L)
 {
 int fn, readed;
 char *buffer;
+
 if (!f_name)
 return (0);
 buffer = malloc(sizeof(char) * L);
 if (!buffer)
 return (0);
+
 fn = open(f_name, O_RDONLY);
 if (fn == -1)
 {
 free(buffer);
 return (0);
 }
-size_t total_read = 0;
-while (total_read < L)
-{
-readed = read(fn, buffer + total_read, L - total_read);
+
+readed = read(fn, buffer, L);
 if (readed == -1)
 {
 free(buffer);
 close(fn);
 return (0);
 }
-if (readed == 0)
-break;
-total_read += readed;
-}
-write(STDOUT_FILENO, buffer, total_read);
+
+write(STDOUT_FILENO, buffer, readed);
+
 free(buffer);
 close(fn);
-return (total_read);
-}
-return (readed);
-}
