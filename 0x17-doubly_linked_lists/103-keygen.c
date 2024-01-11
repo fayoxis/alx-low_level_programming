530fc5880a9b91d2ee91159bba1f7468f70e69bf
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-11 14:06:56 +0100

    Update 103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,64 +1,127 @@
-/*
- * File: 103-keygen.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <time.h>
 
 /**
- * main - Generates and prints passwords for the crackme5 executable.
- * @arg_count: The number of arguments supplied to the program.
- * @argument_v: An array of pointers to the arguments.
+ * f4 - finds the biggest number
  *
- * Return: Always 0.
+ * @usrn: username
+ * @len: length of username
+ * Return: the biggest number
  */
-int main(int __attribute__((__unused__)) arg_count, char *argument_v[])
+int f4(char *usrn, int len)
 {
-    char password[7], *codex;
-    int input_length = strlen(argument_v[1]);
-    int i, index;
+	int ch;
+	int vch;
+	unsigned int rand_num;
 
-    codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	ch = *usrn;
+	vch = 0;
 
-    index = (input_length ^ 59) & 63;
-    password[0] = codex[index];
+	while (vch < len)
+	{
+		if (ch < usrn[vch])
+			ch = usrn[vch];
+		vch += 1;
+	}
 
-    index = 0;
-    for (i = 0; i < input_length; i++)
-        index += argument_v[1][i];
-    password[1] = codex[(index ^ 79) & 63];
+	srand(ch ^ 14);
+	rand_num = rand();
 
-    index = 1;
-    for (i = 0; i < input_length; i++)
-        index *= argument_v[1][i];
-    password[2] = codex[(index ^ 85) & 63];
+	return (rand_num & 63);
+}
 
-    index = 0;
-    for (i = 0; i < input_length; i++)
-    {
-        if (argument_v[1][i] > index)
-            index = argument_v[1][i];
-    }
-    srand(index ^ 14);
-    password[3] = codex[rand() & 63];
+/**
+ * f5 - multiplies each char of username
+ *
+ * @usrn: username
+ * @len: length of username
+ * Return: multiplied char
+ */
+int f5(char *usrn, int len)
+{
+	int ch;
+	int vch;
 
-    index = 0;
-    for (i = 0; i < input_length; i++)
-        index += (argument_v[1][i] * argument_v[1][i]);
-    password[4] = codex[(index ^ 239) & 63];
+	ch = vch = 0;
 
-    index = 0;
-    for (i = 0; i < argument_v[1][0]; i++)
-    {
-        srand(index ^ 14);
-        index = rand();
-    }
-    password[5] = codex[index & 63];
+	while (vch < len)
+	{
+		ch = ch + usrn[vch] * usrn[vch];
+		vch += 1;
+	}
 
-    password[6] = '\0';
-    printf("%s", password);
+	return (((unsigned int)ch ^ 239) & 63);
+}
 
-    return 0;
+/**
+ * f6 - generates a random char
+ *
+ * @usrn: username
+ * Return: a random char
+ */
+int f6(char *usrn)
+{
+	int ch;
+	int vch;
+
+	ch = vch = 0;
+
+	while (vch < *usrn)
+	{
+		ch = rand();
+		vch += 1;
+	}
+
+	return (((unsigned int)ch ^ 229) & 63);
+}
+
+/**
+ * main - Entry point
+ *
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	char keygen[7];
+	int len, ch, vch;
+	long alph[] = {
+		0x3877445248432d41, 0x42394530534e6c37, 0x4d6e706762695432,
+		0x74767a5835737956, 0x2b554c59634a474f, 0x71786636576a6d34,
+		0x723161513346655a, 0x6b756f494b646850 };
+	(void) argc;
+
+	for (len = 0; argv[1][len]; len++)
+		;
+	/* ----------- f1 ----------- */
+	keygen[0] = ((char *)alph)[(len ^ 59) & 63];
+	/* ----------- f2 ----------- */
+	ch = vch = 0;
+	while (vch < len)
+	{
+		ch = ch + argv[1][vch];
+		vch = vch + 1;
+	}
+	keygen[1] = ((char *)alph)[(ch ^ 79) & 63];
+	/* ----------- f3 ----------- */
+	ch = 1;
+	vch = 0;
+	while (vch < len)
+	{
+		ch = argv[1][vch] * ch;
+		vch = vch + 1;
+	}
+	keygen[2] = ((char *)alph)[(ch ^ 85) & 63];
+	/* ----------- f4 ----------- */
+	keygen[3] = ((char *)alph)[f4(argv[1], len)];
+	/* ----------- f5 ----------- */
+	keygen[4] = ((char *)alph)[f5(argv[1], len)];
+	/* ----------- f6 ----------- */
+	keygen[5] = ((char *)alph)[f6(argv[1])];
+	keygen[6] = '\0';
+	for (ch = 0; keygen[ch]; ch++)
+		printf("%c", keygen[ch]);
+	return (0);
 }
