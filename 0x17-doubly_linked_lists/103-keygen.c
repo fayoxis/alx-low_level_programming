af36d059f8ea82dc8142c0ed74c027edeb9383da
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-11 14:19:13 +0100

    Update 103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -6,7 +6,6 @@ int findLargest(char *str, int len)
 {
 int largest = *str;
 int i;
-
 for (i = 1; i < len; i++)
 {
 if (str[i] > largest)
@@ -14,11 +13,9 @@ if (str[i] > largest)
 largest = str[i];
 }
 }
-
 srand(largest ^ 14);
 return ((rand() & 63));
 }
-
 int multiplyChars(char *str, int len)
 {
 int result = 0;
@@ -27,10 +24,8 @@ for (i = 0; i < len; i++)
 {
 result += str[i] * str[i];
 }
-
 return ((result ^ 239) & 63);
 }
-
 int generateRandomChar(char *str)
 {
 int randomChar = 0;
@@ -43,7 +38,6 @@ randomChar = rand();
 
 return ((randomChar ^ 229) & 63);
 }
-
 int main(int argc, char **argv)
 {
 char keygen[7];
@@ -53,15 +47,12 @@ long alph[] = {
 0x74767a5835737956, 0x2b554c59634a474f, 0x71786636576a6d34,
 0x723161513346655a, 0x6b756f494b646850
 };
-
 (void)argc;
-
 len = 0;
 while (argv[1][len])
 {
 len++;
 }
-
 ch = vch = 0;
 while (vch < len)
 {
@@ -70,7 +61,6 @@ vch++;
 }
 keygen[0] = ((char *)alph)[(len ^ 59) & 63];
 keygen[1] = ((char *)alph)[(ch ^ 79) & 63];
-
 ch = 1;
 vch = 0;
 while (vch < len)
@@ -79,12 +69,10 @@ ch = argv[1][vch] * ch;
 vch++;
 }
 keygen[2] = ((char *)alph)[(ch ^ 85) & 63];
-
 keygen[3] = ((char *)alph)[findLargest(argv[1], len)];
 keygen[4] = ((char *)alph)[multiplyChars(argv[1], len)];
 keygen[5] = ((char *)alph)[generateRandomChar(argv[1])];
 keygen[6] = '\0';
-
 ch = 0;
 while (keygen[ch])
 {
