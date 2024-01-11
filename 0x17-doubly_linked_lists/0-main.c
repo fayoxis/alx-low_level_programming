4f88d411e39cd26d85dc4262f21e29bd57c053c6
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-11 10:51:31 +0100

    Update 0-main.c

--- 0x17-doubly_linked_lists/0-main.c
+++ 0x17-doubly_linked_lists/0-main.c
@@ -27,7 +27,7 @@ int main(void)
     new->next = head;
     new->prev = NULL;
     head = NULL;
-    n = print_dlistint(head);
+    n = printDoublyLinkedList(head);
     printf("-> %lu elements\n", n);
     free(new);
     return (EXIT_SUCCESS);
