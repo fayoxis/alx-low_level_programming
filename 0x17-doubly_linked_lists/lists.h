ca22c9322b619283580900f59dfad09714849e4b
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-11 11:04:58 +0100

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -13,12 +13,12 @@
  */
 typedef struct dlistint_s
 {
-    int n;
+    int data;
     struct dlistint_s *prev;
     struct dlistint_s *next;
 } dlistint_t;
 
-size_t printDoublyLinkedList(const dlistint_t *head);
+size_t  DoublyLinkedListNode(const dlistint_t *head);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
