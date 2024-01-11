c314fcadae317187ed3a606dc36abfe8ccfed7ec
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-11 14:10:05 +0100

    Update 103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -2,126 +2,84 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**
- * f4 - finds the biggest number
- *
- * @usrn: username
- * @len: length of username
- * Return: the biggest number
- */
-int f4(char *usrn, int len)
-{
-	int ch;
-	int vch;
-	unsigned int rand_num;
+int findLargest(char *str, int len) {
+    int largest = *str;
+    int i;
 
-	ch = *usrn;
-	vch = 0;
+    for (i = 1; i < len; i++) {
+        if (str[i] > largest) {
+            largest = str[i];
+        }
+    }
 
-	while (vch < len)
-	{
-		if (ch < usrn[vch])
-			ch = usrn[vch];
-		vch += 1;
-	}
-
-	srand(ch ^ 14);
-	rand_num = rand();
-
-	return (rand_num & 63);
+    srand(largest ^ 14);
+    return rand() & 63;
 }
 
-/**
- * f5 - multiplies each char of username
- *
- * @usrn: username
- * @len: length of username
- * Return: multiplied char
- */
-int f5(char *usrn, int len)
-{
-	int ch;
-	int vch;
+int multiplyChars(char *str, int len) {
+    int result = 0;
+    int i;
 
-	ch = vch = 0;
+    for (i = 0; i < len; i++) {
+        result += str[i] * str[i];
+    }
 
-	while (vch < len)
-	{
-		ch = ch + usrn[vch] * usrn[vch];
-		vch += 1;
-	}
-
-	return (((unsigned int)ch ^ 239) & 63);
+    return (result ^ 239) & 63;
 }
 
-/**
- * f6 - generates a random char
- *
- * @usrn: username
- * Return: a random char
- */
-int f6(char *usrn)
-{
-	int ch;
-	int vch;
+int generateRandomChar(char *str) {
+    int randomChar = 0;
+    int i;
 
-	ch = vch = 0;
+    for (i = 0; i < *str; i++) {
+        randomChar = rand();
+    }
 
-	while (vch < *usrn)
-	{
-		ch = rand();
-		vch += 1;
-	}
-
-	return (((unsigned int)ch ^ 229) & 63);
+    return (randomChar ^ 229) & 63;
 }
 
-/**
- * main - Entry point
- *
- * @argc: arguments count
- * @argv: arguments vector
- * Return: Always 0
- */
-int main(int argc, char **argv)
-{
-	char keygen[7];
-	int len, ch, vch;
-	long alph[] = {
-		0x3877445248432d41, 0x42394530534e6c37, 0x4d6e706762695432,
-		0x74767a5835737956, 0x2b554c59634a474f, 0x71786636576a6d34,
-		0x723161513346655a, 0x6b756f494b646850 };
-	(void) argc;
+int main(int argc, char **argv) {
+    char keygen[7];
+    int len, ch, vch;
+    long alph[] = {
+        0x3877445248432d41, 0x42394530534e6c37, 0x4d6e706762695432,
+        0x74767a5835737956, 0x2b554c59634a474f, 0x71786636576a6d34,
+        0x723161513346655a, 0x6b756f494b646850
+    };
 
-	for (len = 0; argv[1][len]; len++)
-		;
-	/* ----------- f1 ----------- */
-	keygen[0] = ((char *)alph)[(len ^ 59) & 63];
-	/* ----------- f2 ----------- */
-	ch = vch = 0;
-	while (vch < len)
-	{
-		ch = ch + argv[1][vch];
-		vch = vch + 1;
-	}
-	keygen[1] = ((char *)alph)[(ch ^ 79) & 63];
-	/* ----------- f3 ----------- */
-	ch = 1;
-	vch = 0;
-	while (vch < len)
-	{
-		ch = argv[1][vch] * ch;
-		vch = vch + 1;
-	}
-	keygen[2] = ((char *)alph)[(ch ^ 85) & 63];
-	/* ----------- f4 ----------- */
-	keygen[3] = ((char *)alph)[f4(argv[1], len)];
-	/* ----------- f5 ----------- */
-	keygen[4] = ((char *)alph)[f5(argv[1], len)];
-	/* ----------- f6 ----------- */
-	keygen[5] = ((char *)alph)[f6(argv[1])];
-	keygen[6] = '\0';
-	for (ch = 0; keygen[ch]; ch++)
-		printf("%c", keygen[ch]);
-	return (0);
+    (void)argc;
+
+    len = 0;
+    while (argv[1][len]) {
+        len++;
+    }
+
+    ch = vch = 0;
+    while (vch < len) {
+        ch += argv[1][vch];
+        vch++;
+    }
+    keygen[0] = ((char *)alph)[(len ^ 59) & 63];
+    keygen[1] = ((char *)alph)[(ch ^ 79) & 63];
+
+    ch = 1;
+    vch = 0;
+    while (vch < len) {
+        ch = argv[1][vch] * ch;
+        vch++;
+    }
+    keygen[2] = ((char *)alph)[(ch ^ 85) & 63];
+
+    keygen[3] = ((char *)alph)[findLargest(argv[1], len)];
+    keygen[4] = ((char *)alph)[multiplyChars(argv[1], len)];
+    keygen[5] = ((char *)alph)[generateRandomChar(argv[1])];
+    keygen[6] = '\0';
+
+    ch = 0;
+    while (keygen[ch]) {
+        printf("%c", keygen[ch]);
+        ch++;
+    }
+
+    return 0;
 }
