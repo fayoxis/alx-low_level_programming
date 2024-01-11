0d13676c2c79b4e0703751bc35d392bdd9d59533
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-11 11:38:03 +0100

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 /**
- * create_node - Creates a new node for a doubly linked list with the given data.
+ * create_node - Creates a new node for a doubly
+ * linked list with the given data.
  * @data: The data to add to the node.
- *
  * Return: A pointer to the newly allocated and populated node.
  */
 dlistint_t *create_node(const int data)
@@ -23,35 +23,35 @@ dlistint_t *create_node(const int data)
 }
 
 /**
- * add_dnodeint - Adds a new node with the given data to the head of a doubly linked list.
+ * add_dnodeint - Adds a new node with the given data
+ * to the head of a doubly linked list.
  * @head: A double pointer to the head of the list.
  * @data: The data to add to the new node.
- *
  * Return: A pointer to the new element, or NULL on failure.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int data)
 {
-    dlistint_t *newNode = NULL, *temp = NULL;
+dlistint_t *newNode = NULL, *temp = NULL;
 
-    newNode = create_node(data);
-    if (!newNode)
-        return (NULL);
+newNode = create_node(data);
+if (!newNode)
+return (NULL);
 
-    if (!head) /* Invalid head pointer */
-        return (NULL);
+if (!head) /* Invalid head pointer */
+return (NULL);
 
-    if (!(*head)) /* Empty DLL */
-    {
-        *head = newNode;
-        return (newNode);
-    }
+if (!(*head)) /* Empty DLL */
+{
+*head = newNode;
+return (newNode);
+}
 
-    temp = *head;
-    while (temp->prev != NULL) /* Traverse to the first node */
-        temp = temp->prev;
+temp = *head;
+while (temp->prev != NULL) /* Traverse to the first node */
+temp = temp->prev;
 
-    temp->prev = newNode;
-    newNode->next = temp;
-    *head = newNode;
-    return (newNode);
+temp->prev = newNode;
+newNode->next = temp;
+*head = newNode;
+return (newNode);
 }
