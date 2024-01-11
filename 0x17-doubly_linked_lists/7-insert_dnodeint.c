045460a1cb6c122c30f4bed139c980cec0cb2fed
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-11 13:07:35 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,74 +2,67 @@
 #include <stdlib.h>
 
 /**
- * create_dnode - creates a new DLL node with data
- * @n: data to add to node
+ * creatednode - it creates a new DLL node with datas
+ * @data: this is data to add to node
  *
- * Return: pointer to newly allocated/populated node
+ * Return: returns pointer to newly allocated/populated node
  */
-dlistint_t *create_dnode(const int n)
+dlistint_t *creatednode(const int data)
 {
-	dlistint_t *new_node = NULL;
-
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	new_node->n = n;
-
-	return (new_node);
+dlistint_t *new_node = NULL;
+new_node = malloc(sizeof(dlistint_t));
+if (!new_node)
+return (NULL);
+new_node->next = NULL;
+new_node->prev = NULL;
+new_node->n = data;
+return (new_node);
 }
 
 /**
- * insert_dnodeint_at_index - inserts a node at position
- * in a doubly linked list
- * @h: double pointer to the head, so we can modify if needed
- * @idx: index to insert new node at
- * @n: data to add to new node
+ * insert_dnodeint_at_index - it  inserts a node at position
+ * in a doubly linked list for operations
+ * @head: it double pointer to the head, so we can modify if needed
+ * @index:it is index to insert new node at
+ * @data: it is data to add to new node
  *
- * Return: pointer to new element, NULL on failure.
+ * Return: returns pointer to new element, NULL on failure.
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int index, int data)
 {
-	dlistint_t *new_node = NULL, *temp = NULL;
-	unsigned int i = 0;
+dlistint_t *new_node = NULL, *current = NULL;
+unsigned int i;
+if (!head)
+return (NULL);
 
-	new_node = create_dnode(n);
-	if (!new_node)
-		return (NULL);
-	if (!h || !(*h)) /* NULL DLL */
-		*h = new_node;
-	else /* DLL exists */
-	{
-		temp = *h;
-		/* advance to pos of idx in DLL */
-		while (idx != i++ && temp->next)
-			temp = temp->next;
-		if (temp->next)
-			new_node->prev = temp->prev;
-		else
-			new_node->prev = temp;
-		if (idx == i) /* only happens when at end of DLL */
-		{
-			temp->next = new_node;
-			new_node->prev = temp;
-		}
-		else if (idx == i - 1) /* insert at head or middle */
-		{
-			if (temp->prev)
-				temp->prev->next = new_node;
-			else /* head of LL */
-				*h = new_node;
-			temp->prev = new_node;
-			new_node->next = temp;
-		}
-		else /* index is out of range */
-		{
-			free(new_node);
-			return (NULL);
-		}
-	}
-	return (new_node);
+new_node = creatednode(data);
+if (!new_node)
+return (NULL);
+if (index == 0)
+{
+new_node->next = *head;
+if (*head)
+(*head)->prev = new_node;
+*head = new_node;
+}
+else
+{
+current = *head;
+for (i = 0; i < index - 1 && current; i++)
+current = current->next;
+if (i == index - 1 && current)
+{
+new_node->next = current->next;
+new_node->prev = current;
+if (current->next)
+current->next->prev = new_node;
+current->next = new_node;
+}
+else
+{
+free(new_node);
+return (NULL);
+}
+}
+return (new_node);
 }
