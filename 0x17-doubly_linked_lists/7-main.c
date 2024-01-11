c32ae04cdcf1a471cec9f542833ba8b9729f8c50
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-11 12:54:49 +0100

    Update 7-main.c

--- 0x17-doubly_linked_lists/7-main.c
+++ 0x17-doubly_linked_lists/7-main.c
@@ -10,22 +10,21 @@
  */
 int main(void)
 {
-    dlistint_t *head;
-
-    head = NULL;
-    add_dnodeint_end(&head, 0);
-    add_dnodeint_end(&head, 1);
-    add_dnodeint_end(&head, 2);
-    add_dnodeint_end(&head, 3);
-    add_dnodeint_end(&head, 4);
-    add_dnodeint_end(&head, 98);
-    add_dnodeint_end(&head, 402);
-    add_dnodeint_end(&head, 1024);
-    print_dlistint(head);
-    printf("-----------------\n");
-    insert_dnodeint_at_index(&head, 99, 4096);
-    print_dlistint(head);
-    free_dlistint(head);
-    head = NULL;
-    return (EXIT_SUCCESS);
+dlistint_t *head;
+head = NULL;
+add_dnodeint_end(&head, 0);
+add_dnodeint_end(&head, 1);
+add_dnodeint_end(&head, 2);
+add_dnodeint_end(&head, 3);
+add_dnodeint_end(&head, 4);
+add_dnodeint_end(&head, 98);
+add_dnodeint_end(&head, 402);
+add_dnodeint_end(&head, 1024);
+print_dlistint(head);
+printf("-----------------\n");
+insert_dnodeint_at_index(&head, 99, 4096);
+print_dlistint(head);
+free_dlistint(head);
+head = NULL;
+return (EXIT_SUCCESS);
 }
