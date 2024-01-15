952921fd28b41fca015cf28662919ac03c348a57
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-15 19:23:31 +0100

    Update operations.c

--- 0x18-dynamic_libraries/operations.c
+++ 0x18-dynamic_libraries/operations.c
@@ -5,61 +5,56 @@ int div(int i, int k);
 int mod(int i, int k);
 
 /**
-* add - adds two integers
-* @i: first integer to add
-* @k: second integer to add
-* Return: the sum
-*/
-
+ * add - adds two integers
+ * @i: first integer to add
+ * @k: second integer to add
+ * Return: the sum
+ */
 int add(int i, int k)
 {
-	return (i + k);
+return (i + k);
 }
 
 /**
-* sub - subtracts two integers
-* @i: first integer to add
-* @k: second integer to add
-* Return: the subtract
-*/
-
+ * sub - subtracts two integers
+ * @i: first integer to subtract from
+ * @k: second integer to subtract
+ * Return: the difference
+ */
 int sub(int i, int k)
 {
-	return (i - k);
+return (i - k);
 }
 
 /**
-* mul - multiplies two integers
-* @i: first integer to add
-* @k: second integer to add
-* Return: the multiple
-*/
-
+ * mul - multiplies two integers
+ * @i: first integer to multiply
+ * @k: second integer to multiply
+ * Return: the product
+ */
 int mul(int i, int k)
 {
-	return (i * k);
+return (i * k);
 }
 
 /**
-* div - divideds two integers
-* @i: first integer to add
-* @k: second integer to add
-* Return: the dividend integer
-*/
-
+ * div - divides two integers
+ * @i: dividend
+ * @k: divisor
+ * Return: the quotient
+ */
 int div(int i, int k)
 {
-	return (i / k);
+return (i / k);
 }
 
 /**
-  mod - finds the modulus of two integers
-  @i: first integer to add
-  @k: second integer to add
-  Return: the modulus
-*/
-
+ * mod - finds the modulus of two integers
+ * @i: dividend
+ * @k: divisor
+ * Return: the modulus
+ */
 int mod(int i, int k)
 {
-	return (i % k);
+return (i % k);
 }
