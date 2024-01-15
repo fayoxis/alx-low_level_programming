181f38bb21b177dec4a712b8eedbe07f459470c8
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-15 22:06:45 +0100

    Update 3-puts.c

--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
+ * _puts - it will print a string to stdout
+ * @str: this is the pointer that point to
+ * the string to print
  */
 void _puts(char *str)
 {
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
