a292e598c9b1a48a3c578a3978bb5ce6f7ebd205
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-15 22:08:12 +0100

    Update 3-strcmp.c

--- 0x18-dynamic_libraries/3-strcmp.c
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * _strcmp - compares two strings
- * @s1: first string to compare
- * @s2: second string to compare
- *
- * Return: less than 0 if s1 is less than s2, 0 if they're equal,
- * more than 0 if s1 is greater than s2
+ * _strcmp - Compares two strings lexicographically.
+ * @s1: The first string to compare.
+ * @s2: The second string to compare.
+ * Returns: An integer less than 0 if s1 is less than s2,
+ * 0 if s1 and s2 are equal,
+ * or an integer greater than 0 if s1 is greater than s2.
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 == *s2)
-	{
-		if (*s1 == '\0')
-		{
-			return (0);
-		}
-		s1++;
-		s2++;
-	}
-	return (*s1 - *s2);
+while (*s1 == *s2)
+{
+if (*s1 == '\0')
+{
+return (0);
+}
+s1++;
+s2++;
+}
+return (*s1 - *s2);
 }
