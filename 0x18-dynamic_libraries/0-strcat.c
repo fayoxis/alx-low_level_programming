41d6f7484c6b3083ae91f9ad5d51fd67490bfe84
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-15 21:54:53 +0100

    Update 0-strcat.c

--- 0x18-dynamic_libraries/0-strcat.c
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -8,22 +8,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+int i, j;
 
-	i = 0;
-	j = 0;
+i = 0;
+j = 0;
 
-	while (dest[i] != '\0')
-		i++;
+while (dest[i] != '\0')
+i++;
 
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
 
-	dest[i] = '\0';
-
-	return (dest);
+dest[i] = '\0';
+return (dest);
 }
