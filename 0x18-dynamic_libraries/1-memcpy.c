7ab2b0a88120157fef19fafccb0c12e96228a6d6
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-15 21:56:39 +0100

    Update 1-memcpy.c

--- 0x18-dynamic_libraries/1-memcpy.c
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -1,21 +1,18 @@
 #include "main.h"
 
 /**
- * *_memcpy - copies memory area
- * @dest: destination memory area
- * @src: memory area to copy from
- * @n: number of bytes to copy
- *
- * Return: pointer to dest
+ * _memcpy - Copies a memory area.
+ * @dest: The destination memory area.
+ * @src: The memory area to copy from.
+ * @n: The number of bytes to copy.
+ * Returns: A pointer to the destination memory area.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-
-	return (dest);
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
 }
