5f8a683ba2b9f6db91838163ab4be776cbe91579
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-15 22:03:15 +0100

    Update 2-strlen.c

--- 0x18-dynamic_libraries/2-strlen.c
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -1,21 +1,18 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
+ * _strlen - this will returns the length of a string
+ * @s: this is the string to be evaluate
  *
- * Return: the length of the string
+ * Return: returns the length of the string
  */
 int _strlen(char *s)
 {
-	int i;
-
-	i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-
-	return (i);
+int i;
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
 }
