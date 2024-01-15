2d0a5baaa16d293d6e65429b8200dc22d08ab777
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-15 21:54:13 +0100

    Update 1-isdigit.c

--- 0x18-dynamic_libraries/1-isdigit.c
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for a digit (0 through 9)
- * @c: int to be checked
- * Return: 1 if c is a digit, 0 otherwise
+ * _isdigit - it checks for a digit (0 through 9)
+ * @c: thi is the int to be checked
+ * Return: returns  1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-	return (c >= '0' && c <= '9');
+return (c >= '0' && c <= '9');
 }
