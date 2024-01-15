02eedb6db3702ee044bf3b4b477a5772a681bf57
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-15 22:15:24 +0100

    Update 5-strstr.c

--- 0x18-dynamic_libraries/5-strstr.c
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -2,26 +2,24 @@
 #include <stdio.h>
 
 /**
- * *_strstr - locates a substring
- * @haystack: string to search in
- * @needle: substring to look for
- *
- * Return: pointer to the beginning of the located substring
+ * *_strstr - it alocates a substring
+ * @haystack: this is the string to be search 
+ * @needle: thi sis the substring to look for
+ * Return: return pointer to the beginningof located substring
  * or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
-
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		for (j = 0; needle[j] != '\0'; j++)
-		{
-			if (haystack[i + j] != needle[j])
-				break;
-		}
-		if (!needle[j])
-			return (&haystack[i]);
-	}
-	return (NULL);
+int i, j;
+for (i = 0; haystack[i] != '\0'; i++)
+{
+for (j = 0; needle[j] != '\0'; j++)
+{
+if (haystack[i + j] != needle[j])
+break;
+}
+if (!needle[j])
+return (&haystack[i]);
+}
+return (NULL);
 }
