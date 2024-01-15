50dc639e450653f3f9011c755b52fbdeba1b59a6
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-15 22:53:09 +0100

    Update 0-strcat.c

--- 0x18-dynamic_libraries/0-strcat.c
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * *_strcat - Concatenates two strings.
+ * _strcat - concatenates two of the strings
  * @dest: The string to which the second string will be appended.
- * @src: The string that will be added to the end of the first string.
+ * @src: The string that will be added to the end of the first string
  *
- * Returns: A pointer to the resulting string after concatenation.
+ * Return: returns a pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
 {
