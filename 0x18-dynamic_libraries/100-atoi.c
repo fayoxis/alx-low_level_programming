38fa2684414e74967d1942559cd62f261acd4054
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-15 22:33:54 +0100

    Update 100-atoi.c

--- 0x18-dynamic_libraries/100-atoi.c
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -8,33 +8,29 @@
  */
 int _atoi(char *s)
 {
-    int n = 0;
-    int sign = 1;
-    int i = 0;
-
-    /* Skip leading whitespace */
-    while (s[i] == ' ')
-    {
-        i++;
-    }
-
-    /* Check the sign */
-    if (s[i] == '-')
-    {
-        sign = -1;
-        i++;
-    }
-    else if (s[i] == '+')
-    {
-        i++;
-    }
-
-    /* Convert digits to integer */
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        n = n * 10 + (s[i] - '0');
-        i++;
-    }
-
-    return n * sign;
+int n = 0;
+int si = 1;
+int i = 0;
+/* Skip leading whitespace */
+while (s[i] == ' ')
+{
+i++;
+}
+/* Check the si */
+if (s[i] == '-')
+{
+si= -1;
+i++;
+}
+else if (s[i] == '+')
+{
+i++;
+}
+/* Convert digits to integer */
+while (s[i] >= '0' && s[i] <= '9')
+{
+n = n * 10 + (s[i] - '0');
+i++;
+}
+return (n * si);
 }
