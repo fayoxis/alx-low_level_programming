46f8aa380abfe2863dac76b149504a1b4523c571
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-19 21:48:44 +0100

    Create 0-isupper.c

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isupper - checks if the character is  uppercase character
+ * @c: this is the  character to be checked
+ * Return: returns 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+return (c >= 'A' && c <= 'Z');
+}
