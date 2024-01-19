17d5db14a23914828dc5486d7c2885923a53a151
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-19 20:25:23 +0100

    Update 100-atoi.c

--- 0x18-dynamic_libraries/100-atoi.c
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -1,46 +1,42 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
+ * _atoi - it converts a string to an integer
+ * @s: it string to be converted
  *
- * Return: the int converted from the string
+ * Return: returns the int converted from the string
  */
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+int i, d, n, len, f, digit;
+i = 0;
+d = 0;
+num = 0;
+length = 0;
+f = 0;
+digit = 0;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+if (s[length] != '\0')
+length++;
+while (i < length && f == 0)
+{
+if (s[i] == '-')
+++d;
 
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
+if (s[i] >= '0' && s[i] <= '9')
+{
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+num = num * 10 + digit;
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
+f = 0;
+}
+i++;
+}
+while (f == 0)
+return (0);
+return (num);
 }
