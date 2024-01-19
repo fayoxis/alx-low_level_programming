99ceeb1ecbe5b4f7d91266bcf22462ea94d44430
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-19 20:15:59 +0100

    Update operations.c

--- 0x18-dynamic_libraries/operations.c
+++ 0x18-dynamic_libraries/operations.c
@@ -1,43 +1,43 @@
-int add(int i, int k);
-int sub(int i, int k);
-int mul(int i, int k);
+int add(int k, int i);
+int sub(int j, int k);
+int mul(int i, int j);
 int div(int i, int k);
-int mod(int i, int k);
+int mod(int k, int j);
 
 /**
 * add - adds two integers
-* @i: first integer to add
-* @k: second integer to add
+* @k: first integer to add
+* @i: second integer to add
 * Return: the sum
 */
 
-int add(int i, int k)
+int add(int k, int i)
 {
-	return (i + k);
+return (k + i);
 }
 
 /**
 * sub - subtracts two integers
-* @i: first integer to add
+* @j: first integer to add
 * @k: second integer to add
 * Return: the subtract
 */
 
-int sub(int i, int k)
+int sub(int j, int k)
 {
-	return (i - k);
+return (j - k);
 }
 
 /**
 * mul - multiplies two integers
 * @i: first integer to add
-* @k: second integer to add
+* @j: second integer to add
 * Return: the multiple
 */
 
-int mul(int i, int k)
+int mul(int i, int j)
 {
-	return (i * k);
+return (i * j);
 }
 
 /**
@@ -49,17 +49,17 @@ int mul(int i, int k)
 
 int div(int i, int k)
 {
-	return (i / k);
+return (i / k);
 }
 
 /**
-  mod - finds the modulus of two integers
-  @i: first integer to add
-  @k: second integer to add
-  Return: the modulus
-*/
+ * mod - finds the modulus of two integers
+ * @k: first integer to add
+ * @j: second integer to add
+ * Return: the modulus
+ */
 
-int mod(int i, int k)
+int mod(int k, int j)
 {
-	return (i % k);
+return (i % k);
 }
