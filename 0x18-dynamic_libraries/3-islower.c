25c7c2933bceb9bb7745140b01ede9ae57954038
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-19 20:30:27 +0100

    Update 3-islower.c

--- 0x18-dynamic_libraries/3-islower.c
+++ 0x18-dynamic_libraries/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character
- * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
+ * _islower - it checks for lowercase character
+ * @c: this is  the character to check
+ * Return: returns 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
-	return (c >= 'a' && c <= 'z');
+return (c >= 'a' && c <= 'z');
 }
