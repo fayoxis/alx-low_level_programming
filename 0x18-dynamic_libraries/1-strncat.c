bcb674053214e302006a6a34bcfcf476bc3f300b
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-19 20:22:34 +0100

    Update 1-strncat.c

--- 0x18-dynamic_libraries/1-strncat.c
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -1,31 +1,31 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates n bytes from a string to another
- * @dest: destination string
- * @src: source string
- * @n: number of bytes of str to concatenate
+ * _strncat - it concatenates n bytes from a string to another
+ * @dest: this is the destination of the string
+ * @src: it is the source string
+ * @n: the number of bytes of str to concatenate
  *
- * Return: a pointer to the resulting string dest
+ * Return: returns a pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+int i, k;
 
-	i = 0;
-	j = 0;
+i = 0;
+k = 0;
 
-	while (dest[i] != '\0')
-		i++;
+while (dest[i] != '\0')
+i++;
 
-	while (src[j] != '\0' && j < n)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
+if (src[k] != '\0' && k < n)
+{
+dest[i] = src[k];
+i++;
+k++;
+}
 
-	dest[i] = '\0';
+dest[i] = '\0';
 
-	return (dest);
+return (dest);
 }
