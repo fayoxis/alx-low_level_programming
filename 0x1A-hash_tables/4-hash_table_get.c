f65ae6476bb7c0aa3eac1b53d9b3868c9502398c
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-02 23:24:30 +0100

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -20,7 +20,7 @@ current_node = ht->array[index];
 while (current_node != NULL)
 {
 if (strcmp(current_node->key, key) == 0)
-return current_node->value;
+return (current_node->value);
 current_node = current_node->next;
 }
 return (NULL);
