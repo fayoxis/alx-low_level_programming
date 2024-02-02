2ddc88c9450a6aff029869d6b6e469ff5c91d21e
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-02 23:16:51 +0100

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -73,17 +73,23 @@ typedef struct shash_table_s
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+
 hash_node_t *_node_(const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 shash_table_t *shash_table_create(unsigned long int size);
+
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
+
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
+
 void shash_table_delete(shash_table_t *ht);
 shash_node_t *make_shash_node(const char *key, const char *value);
 void add_to_sorted_list(shash_table_t *table, shash_node_t *node);
