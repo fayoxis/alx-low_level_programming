c4fd4249859d19c3d303346952cb46040da453a9
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-02 22:43:52 +0100

    Update 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,8 +6,8 @@
  * @size: Size of the hash table array.
  *
  * This function uses the hash_djb2 function to calculate the index.
- *
- * Return: Index at which the key/value pair should be stored in the hash table array.
+ * Return: Index at which the key/value pair should be
+ * stored in the hash table array.
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
