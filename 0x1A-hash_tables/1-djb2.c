554c97f54dccd21ca25a877539643948e4cc9aaf
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-02 22:35:27 +0100

    Update 1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,20 +1,19 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - hash function to implement djb2 algorithm.
- * @str: pointer to null-terminated byte string to be examined.
+ * hash_djb2 - Implements the djb2 algorithm for hashing.
+ * @str: Pointer to a null-terminated byte string to be hashed.
  *
- * Return: The new hash.
+ * Return: The resulting hash value.
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash;
-	int c;
-
-	hash = 5381;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	return (hash);
+unsigned long int hash = 5381;
+int com;
+for (; *str != '\0'; str++)
+{
+com = *str;
+hash = ((hash << 5) + hash) + com;
+}
+return (hash);
 }
