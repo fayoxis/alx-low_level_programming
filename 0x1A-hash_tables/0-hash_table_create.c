31fadd0c50a4a05cd6a0f095edacaafa1229a424
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-02 22:30:30 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,26 +11,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table = NULL;
-    unsigned long int i;
+hash_table_t *new_table = NULL;
+unsigned long int i;
 
-    if (size == 0)
-        return NULL;
+if (size == 0)
+return (NULL);
+new_table = malloc(sizeof(hash_table_t));
+if (new_table == NULL)
+return (NULL);
 
-    new_table = malloc(sizeof(hash_table_t));
-    if (new_table == NULL)
-        return (NULL);
-
-    new_table->size = size;
-    new_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (new_table->array == NULL) {
-        free(new_table);
-        return (NULL);
-    }
-
-    for (i = 0; i < size; i++) {
-        new_table->array[i] = NULL;
-    }
-
-    return (new_table);
+new_table->size = size;
+new_table->array = malloc(sizeof(hash_node_t *) * size);
+if (new_table->array == NULL) {
+free(new_table);
+return (NULL);
+}
+for (i = 0; i < size; i++) {
+new_table->array[i] = NULL;
+}
+return (new_table);
 }
