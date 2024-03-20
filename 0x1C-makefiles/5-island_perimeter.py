8f18f9c9f8e9ee54b2a9713e0e2483d37ef1ba61
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-03-20 16:57:04 +0100

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,9 +12,9 @@ def island_perimeter(grid):
     land, neighbour = 0, 0
     for i in range(m):
         for j in range(n):
-            if grid[i][j] == 1:
+            while grid[i][j] == 1:
                 land += 1
-                if i < m - 1 and grid[i+1][j] == 1:
+                while i < m - 1 and grid[i+1][j] == 1:
                     neighbour += 1
                 if j < n - 1 and grid[i][j + 1] == 1:
                     neighbour += 1
