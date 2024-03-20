3a6990e55f55de788e7383969cbc8f9436968af3
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-03-20 16:50:34 +0100

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,21 +1,31 @@
-#!/usr/bin/python3
-"""Function island_perimeter(grid)"""
-
-
 def island_perimeter(grid):
-    """returns the perimeter of the island described in grid.
+    """Returns the perimeter of the island in grid."""
 
-    Args:
-        grid (list): grid
-    """
-    m, n = len(grid), len(grid[0])
-    land, neighbour = 0, 0
-    for i in range(m):
-        for j in range(n):
-            if grid[i][j] == 1:
-                land += 1
-                if i < m - 1 and grid[i+1][j] == 1:
-                    neighbour += 1
-                if j < n - 1 and grid[i][j + 1] == 1:
-                    neighbour += 1
-    return land * 4 - 2 * neighbour
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+    i = 0
+
+    while i < rows:
+        j = 0
+
+        while j < cols:
+            if grid[i][j]:
+                num = 0
+
+                if i <= 0 or not grid[i - 1][j]:
+                    num += 1
+                if j <= 0 or not grid[i][j - 1]:
+                    num += 1
+                if j >= cols - 1 or not grid[i][j + 1]:
+                    num += 1
+                if i >= rows - 1 or not grid[i + 1][j]:
+                    num += 1
+
+                perimeter += num
+
+            j += 1
+
+        i += 1
+
+    return perimeter
