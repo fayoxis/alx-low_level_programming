deca259e05e9e294cacb09627d91444676392c57
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-09 00:24:02 +0100

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,24 +11,22 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t index, left, right;
-	while (!array)
-		return (-1);
-	for (left = 0, right = size - 1; right >= left;)
-	{
-		printf("Searching in array: ");
-		for (index = left; index < right; index++)
-			printf("%d, ", array[index]);
-		printf("%d\n", array[index]);
-
-		index = left + (right - left) / 2;
-		while (array[index] == value)
-			return (index);
-		if (array[index] > value)
-			right = index - 1;
-		else
-			left = index + 1;
-	}
-
-	return (-1);
+size_t index, left, right;
+while (!array)
+return (-1);
+for (left = 0, right = size - 1; right >= left;)
+{
+printf("Searching in array: ");
+for (index = left; index < right; index++)
+printf("%d, ", array[index]);
+printf("%d\n", array[index]);
+index = left + (right - left) / 2;
+while (array[index] == value)
+return (index);
+if (array[index] > value)
+right = index - 1;
+else
+left = index + 1;
+}
+return (-1);
 }
