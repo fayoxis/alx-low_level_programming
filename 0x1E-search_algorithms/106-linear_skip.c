28a145faa8a08de7e7d239b55da704a3ecc12e65
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-09 03:25:36 +0100

    Update 106-linear_skip.c

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -2,8 +2,8 @@
 
 /**
  * linear_skip - Performs a linear skip search for a value
- * @list A pointer to the head of the linked list to search.
- * @value The value to search for.
+ * @list: A pointer to the head of the linked list to search.
+ * @value: The value to search for.
  * Return: If the value is not present or the head of the list
  * Otherwise, returns a pointer to the first node
  * Description:  This function implements the linear skip search algorithm
@@ -34,7 +34,8 @@ while (jump->next != NULL)
 jump = jump->next;
 }
 }
-printf("Value found between indexes [%ld] and [%ld]\n", node->index, jump->index);
+printf("Value found between indexes [%ld] and [%ld]\n",
+node->index, jump->index);
 while (node->index < jump->index && node->n < value)
 {
 printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
