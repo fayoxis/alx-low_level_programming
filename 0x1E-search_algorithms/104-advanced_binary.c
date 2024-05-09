36343eb327ef111994fd8a7649c204a2f9d6068b
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-09 02:04:55 +0100

    Update 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -22,9 +22,9 @@ for (index = left; index < right; index++)
 printf("%d, ", array[index]);
 printf("%d\n", array[index]);
 index = left + (right - left) / 2;
-while (array[index] == value && (index == left || array[index - 1] != value))
+if (array[index] == value && (index == left || array[index - 1] != value))
 return (index);
-while (array[index] >= value)
+if (array[index] >= value)
 return (advan_bin_recursive(array, left, index, value));
 return (advan_bin_recursive(array, index + 1, right, value));
 }
@@ -41,7 +41,7 @@ return (advan_bin_recursive(array, index + 1, right, value));
   */
 int advanced_binary(int *array, size_t size, int value)
 {
-	while (array == NULL || size == 0)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	return (advan_bin_recursive(array, 0, size - 1, value));
