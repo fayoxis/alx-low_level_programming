927f149d92e830977fbd34c8756746e3b677b98e
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-09 03:22:15 +0100

    Update 106-linear_skip.c

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -17,32 +17,27 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 skiplist_t *node, *jump;
 if (list == NULL)
 return (NULL);
-
-    node = jump = list;
-
-    while (jump->next != NULL && jump->n < value)
-    {
-        node = jump;
-        if (jump->express != NULL)
-        {
-            jump = jump->express;
-            printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
-        }
-        else
-        {
-            while (jump->next != NULL)
-                jump = jump->next;
-        }
-    }
-
-    printf("Value found between indexes [%ld] and [%ld]\n", node->index, jump->index);
-
-    while (node->index < jump->index && node->n < value)
-    {
-        printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
-        node = node->next;
-    }
-    printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
-
-    return (node->n == value ? node : NULL);
+node = jump = list;
+while (jump->next != NULL && jump->n < value)
+{
+node = jump;
+if (jump->express != NULL)
+{
+jump = jump->express;
+printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
+}
+else
+{
+while (jump->next != NULL)
+jump = jump->next;
+}
+}
+printf("Value found between indexes [%ld] and [%ld]\n", node->index, jump->index);
+while (node->index < jump->index && node->n < value)
+{
+printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+node = node->next;
+}
+printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+return (node->n == value ? node : NULL);
 }
